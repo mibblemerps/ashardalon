80930ba25fb275a80c952dd45543e8356dd3fe24
Mitchfizz05 <me@mitchfizz05.net>
2016-08-28 23:52:04 +0930

    Added a Pixi.js test (a rotating Rainbow Dash). Will be removed, obviously.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log("Hello world");
+require("pixi.js");
+
+// Init
+var stage = new PIXI.Stage(0x66FF99);
+var renderer = PIXI.autoDetectRenderer(800, 600);
+
+document.body.appendChild(renderer.view);
+
+requestAnimationFrame(animate);
+
+// Add a dashpone for testing :3
+var texture = PIXI.Texture.fromImage("dashpone.png");
+var dashpone = new PIXI.Sprite(texture);
+dashpone.anchor.x = 0.5;
+dashpone.anchor.y = 0.5;
+dashpone.position.x = 200;
+dashpone.position.y = 200;
+stage.addChild(dashpone);
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    dashpone.rotation += 0.1;
+
+    renderer.render(stage);
+}
+
