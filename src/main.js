f283245e6bc2fcd7f608510ae3740e36967d680d
Mitchfizz05 <me@mitchfizz05.net>
2016-08-29 04:20:45 +0930

    We're now simply rendering a game tile onto the screen.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 require("pixi.js");
 
 // Init
-var stage = new PIXI.Stage(0x66FF99);
+var stage = new PIXI.Container();
 var renderer = PIXI.autoDetectRenderer(800, 600);
 
 document.body.appendChild(renderer.view);
@@ -9,18 +9,12 @@ document.body.appendChild(renderer.view);
 requestAnimationFrame(animate);
 
 // Add a dashpone for testing :3
-var texture = PIXI.Texture.fromImage("dashpone.png");
-var dashpone = new PIXI.Sprite(texture);
-dashpone.anchor.x = 0.5;
-dashpone.anchor.y = 0.5;
-dashpone.position.x = 200;
-dashpone.position.y = 200;
-stage.addChild(dashpone);
+var testTileTexture = PIXI.Texture.fromImage("assets/tiles/white01.png");
+var testTile = new PIXI.Sprite(testTileTexture);
+stage.addChild(testTile);
 
 function animate() {
     requestAnimationFrame(animate);
-
-    dashpone.rotation += 0.1;
 
     renderer.render(stage);
 }
