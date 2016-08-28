4806048279b6a7ec577de1d484e8b28379c4bc7d
Mitchfizz05 <me@mitchfizz05.net>
2016-08-29 05:46:04 +0930

    Placed tiles are now rendered.

--- src/Tile.js
+++ src/Tile.js
@@ -2,9 +2,11 @@
  * Represents a game tile.
  */
 class Tile {
-    constructor() {
-        this.position = {x: 0, y: 0};
-
+    constructor(tileType, position = {x: 0, y: 0}) {
+        this.tileType = tileType;
+        this.position = position;
+        this.size = {width: 1, height: 1};
+        this.orientation = 0;
     }
 }
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
 require("pixi.js");
 
 import TileType from "./TileType";
+import Tile from "./Tile";
 
 // Constants
 const ASSET_URL = "assets";
+const TILE_PIXELS = {width: 256, height: 256}; // size of each tile size unit in pixels. Virtually all tiles are 1x1 tile units in size.
 
 var tileTypes;
+
+// Array of all the placed tiles.
+var placedTiles = [];
+
+var placedTileSprites = [];
 
 /**
  * Load game tile types...
@@ -21,30 +28,58 @@ function loadTileTypes() {
         var tileTypeDef = tileTypeConfig[tileTypeId];
         var id = parseInt(tileTypeId);
 
-        tileTypes[id] = new TileType(id, tileTypeDef.texture, tileTypeDef.encounter);
+        var tileTexture = PIXI.Texture.fromImage(ASSET_URL + "/tiles/" + tileTypeDef.texture);
+        tileTypes[id] = new TileType(id, tileTexture, tileTypeDef.encounter);
     }
 
     console.log("Loaded " + Object.keys(tileTypes).length + " tiles!");
 
     return tileTypes;
 }
+// Load tile types
 tileTypes = loadTileTypes();
+
+// Place initial tile
+placedTiles.push(new Tile(1, {x: 0, y: 0}));
+placedTiles.push(new Tile(1, {x: 0, y: 1}));
 
 // Init rendering
 var stage = new PIXI.Container();
 var renderer = PIXI.autoDetectRenderer(800, 600);
 document.body.appendChild(renderer.view);
 
+/**
+ * Render placed tiles.
+ */
+function renderPlacedTileSprites() {
+    // Remove old tile sprites
+    for (var spriteId in placedTileSprites) {
+        stage.removeChild(placedTileSprites[spriteId]);
+    }
+    placedTileSprites = [];
+
+    // Generate new sprites.
+    placedTiles.forEach(function (placedTile) {
+        // Create new sprite for this tile.
+        var newSprite = new PIXI.Sprite(tileTypes[placedTile.tileType].texture);
+        //console.log(newSprite);
+        newSprite.x = placedTile.position.x * TILE_PIXELS.width;
+        newSprite.y = placedTile.position.y * TILE_PIXELS.height;
+        newSprite.width = TILE_PIXELS.width;
+        newSprite.height = TILE_PIXELS.height;
+
+        placedTileSprites.push(newSprite);
+        stage.addChild(newSprite);
+    });
+}
+
+// Begin render loop.
 requestAnimationFrame(animate);
-
-// Add a test tile for testing.
-var testTileTexture = PIXI.Texture.fromImage(ASSET_URL + "/tiles/" + tileTypes[1].texture);
-var testTile = new PIXI.Sprite(testTileTexture);
-stage.addChild(testTile);
-
 function animate() {
-    requestAnimationFrame(animate);
+    // Render placed tiles.
+    renderPlacedTileSprites();
 
+    requestAnimationFrame(animate);
     renderer.render(stage);
 }
 
