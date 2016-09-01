b099d8c908fd3f52547dfc35de9a0dc74334e6aa
Mitchfizz05 <me@mitchfizz05.net>
2016-09-02 01:55:33 +0930

    Entity icon displayed in entity info modal is now fetched from the entity type.

--- src/Ui/EntityInfoModal.js
+++ src/Ui/EntityInfoModal.js
@@ -19,6 +19,8 @@ class EntityInfoModal {
         modal.find(".entityMeta-subtitle").html(this.entity.entityType.subtitle);
         modal.find(".stat-armour").html(this.entity.entityType.armor);
         modal.find(".stat-hp").html(this.entity.entityType.health);
+        modal.find(".entityIcon")[0].src = this.entity.entityType.texture.baseTexture.imageUrl;
+
     }
 }
 
