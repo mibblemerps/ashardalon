6cac07bad280b78ce04a57b4815586c74486cd22
Mitchfizz05 <me@mitchfizz05.net>
2016-09-02 03:39:39 +0930

    HP tokens are now displayed on the entity info window.

--- src/Ui/EntityInfoModal.js
+++ src/Ui/EntityInfoModal.js
@@ -23,6 +23,18 @@ class EntityInfoModal {
         modal.find(".stat-surge").html(this.entity.entityType.surgeValue + " HP");
         modal.find(".entityIcon")[0].src = this.entity.entityType.texture.baseTexture.imageUrl;
 
+        // Render HP points
+        var hitsTaken = this.entity.entityType.health - this.entity.health;
+        var hp5s = Math.floor(hitsTaken / 5);
+        var hp1s = hitsTaken % 5;
+        console.log("HP5s: " + hp5s + "  HP1s:" + hp1s);
+        var healthBar = modal.find(".entityMeta-health");
+        for (var i = 0; i < hp5s; i++) {
+            healthBar.append("<img src='assets/tokens/5hp.png' alt='5HP' class='entityMeta-health-hp'>");
+        }
+        for (var i = 0; i < hp1s; i++) {
+            healthBar.append("<img src='assets/tokens/1hp.png' alt='1HP' class='entityMeta-health-hp'>");
+        }
     }
 }
 
